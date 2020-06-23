6a4c0ed3eb7e8867ff7fe4614dbb715de7fe02e3
amitchai <amit@wingsgames.com>
2020-06-24 00:40:18 +0300

    fix script

--- Example Game Project/Assets/Scenes/test.cs
+++ Example Game Project/Assets/Scenes/test.cs
@@ -5,15 +5,16 @@ using UnityEngine;
 public class test : MonoBehaviour
 {
     // Start is called before the first frame update
+    bool tests;
     void Start()
     {
-<<<<<<< HEAD
+
         tests = true;
 
         tests = false;
-=======
+
         
->>>>>>> parent of 7d350b2... 1
+
     }
 
     // Update is called once per frame
