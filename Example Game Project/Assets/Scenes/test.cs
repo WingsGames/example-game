7d350b23916cf3894192c57b5aec19a07060e3c1
amitchai <amit@wingsgames.com>
2020-06-24 00:19:29 +0300

    1

--- Example Game Project/Assets/Scenes/test.cs
+++ Example Game Project/Assets/Scenes/test.cs
@@ -5,9 +5,10 @@ using UnityEngine;
 public class test : MonoBehaviour
 {
     // Start is called before the first frame update
+    bool tests;
     void Start()
     {
-        
+        tests = true;
     }
 
     // Update is called once per frame
